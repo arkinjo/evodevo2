f2d824a2e9fefcc780f23fbaa95a7effecca385f
Akira R Kinjo <993982+arkinjo@users.noreply.github.com>
2023-07-18 12:38:00 +0800

    Update hoge.jl
    ThreadsX vs Threads

--- src/hoge.jl
+++ src/hoge.jl
@@ -1,5 +1,6 @@
 using JLD2,CodecZlib
 include("EvoDevo2.jl")
+using ThreadsX
 
 s = default_setting("full", 5, 13)
 s.max_pop = 1000
@@ -20,8 +21,8 @@ if ARGS[1] == "no"
             develop(indiv, envs1, s)
         end
     end
-else
-    println("### With threads ###")
+elseif ARGS[1] == "t"
+    println("### With @threads ###")
     pop = Population(Novel, s);
     for i = 1:10
         @time Threads.@threads for indiv in pop.indivs
@@ -29,9 +30,14 @@ else
             develop(indiv, envs1, s)
         end
     end
+else
+    println("### With ThreadsX ###")
+    pop = Population(Novel, s);
+    for i = 1:10
+        @time ThreadsX.foreach(pop.indivs) do indiv
+            set_cues(indiv, envs1, s)
+            develop(indiv, envs1, s)
+        end
+    end
 end
 
-a = ones(200);
-b = map(a[1:100], a[101:200]) do x,y
-    x + y
-end
