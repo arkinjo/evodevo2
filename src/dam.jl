de2f7248a1da75fb69acc5c03938a03f189c694b
Akira R Kinjo <993982+arkinjo@users.noreply.github.com>
2023-09-04 05:43:09 +0800

    renamed DAM

--- src/dam.jl
+++ src/dam.jl
@@ -5,49 +5,38 @@ using Random
 using LinearAlgebra
 using Printf
 
-struct HopEnv
+"Dense Associative Memory"
+struct DAM
     seed::Int64
     ndim::Int64
-    npat::Int64
     patterns::Vector{Vector{Float32}}
-    W::Matrix{Float32}
 end
 
-function HopEnv(seed, ndim, npat)
+function DAM(seed, ndim, npat)
     Random.seed!(seed)
     patterns = Vector{Vector{Float32}}()
-    W = zeros(Float32, ndim, ndim)
     for k = 1:npat
         v = rand([-1.0f0, 1.0f0], ndim)
         push!(patterns, v)
-        W += v*v' 
     end
-    W[diagind(W)] .= 0.0
-    W /= npat
-    HopEnv(seed, ndim, npat, patterns, W)
+    DAM(seed, ndim, patterns)
 end
 
-function flip1(hop::HopEnv, env, i, β)
-    w = hop.W[i,:] ⋅ env
-    if rand() < exp(-β*env[i]*w) # Metropolis
-            env[i] *= -1
-    end
+function numpat(hop::DAM)
+    length(hop.patterns)
 end
 
-function flipn(hop::HopEnv, n, β, env)
-    for i in shuffle(1:n)
-        flip1(hop, env, i, β)
-    end
+function addpat(hop::DAM, pat)
+    push!(hop.patterns, pat)
 end
 
-function DAMenergy(hop::HopEnv, env)
-    energy = mapreduce(ξ -> exp(ξ ⋅ env/2√hop.ndim), +, hop.patterns)
-    energy *= -1/hop.npat
+function energy(hop::DAM, env)
+    -mapreduce(ξ -> exp(ξ ⋅ env/2√hop.ndim), +, hop.patterns)/length(hop.patterns)
 end
 
-function DAMflip1(hop::HopEnv, env, i, β)
+function flip1(hop::DAM, env, i, β)
     ene0 = DAMenergy(hop, env)
-    env[i] *= -1
+    env[i] *= -1 # flip
     ene1 = DAMenergy(hop, env)
     dene = ene1 - ene0
     if dene > 0.0 && rand() > exp(-β*dene) # Metropolis
@@ -55,14 +44,14 @@ function DAMflip1(hop::HopEnv, env, i, β)
     end
 end
 
-function DAMflip(hop::HopEnv, env, β)
+function flip(hop::DAM, env, β)
     for i in shuffle(1:hop.ndim)
         DAMflip1(hop, env, i, β)
     end
 end
 
 "batch-sampling all elements at once."
-function DAMflip_batch(hop::HopEnv, env, β)
+function flip_batch(hop::DAM, env, β)
     dots = map(ξ -> ξ ⋅ env, hop.patterns)
     ene0 = -mapreduce(d -> exp(d/2√hop.ndim), +, dots)/hop.npat
     for (i,e) = enumerate(env)
@@ -78,13 +67,13 @@ end
 "for testing"
 function checkhop(hop, ipat, beta, flip)
     env0 = ipat > 0 ?  hop.patterns[ipat] : rand([-1.0f0, 1.0f0], 200)
-    ene0 = DAMenergy(hop, env0)
+    ene0 = energy(hop, env0)
     devs = Vector()
     envs = Vector()
     for k = 1:500
         env = copy(env0);
         flip(hop, env, beta)
-        dene = DAMenergy(hop, env) - ene0
+        dene = energy(hop, env) - ene0
         dev = norm((env - env0)/2, 1)
         push!(envs, env)
         push!(devs, dev)
